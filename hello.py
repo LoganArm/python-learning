5da0c1d66bb0e915f1eef1cee5753af005edf2ae
Logan Armstrong <LSArmstrong01@indianatech.net>
2025-08-21 08:14:05 -0400

    Initial commit - first Python project

---
+++ hello.py
@@ -0,0 +1,6 @@
+# My first Python Program
+name = input("What's your name? ")
+print(f"Hello, {name} ! Welcome to Puthon programming!")
+print("Python version check:")
+import sys
+print(sys.version)
